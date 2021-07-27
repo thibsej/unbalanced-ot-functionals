ef76a15065bb7d1d7dd867dfc4d0f717cd0ec82e
thibsej <thibault.sejourne@ens.fr>
2021-07-27 14:57:38 +0200

    Add meta files to set up a pip package

--- setup.py
+++ setup.py
@@ -1,65 +1,19 @@
 #! /usr/bin/env python
 
-import os
-import setuptools  # noqa; we are using a setuptools namespace
-from numpy.distutils.core import setup
-
-descr = """Functionals derived from the theory of entrpically regularized unbalanced optimal transport"""
-
-version = None
-with open(os.path.join('common', '__init__.py'), 'r') as fid:
-    for line in (line.strip() for line in fid):
-        if line.startswith('__version__'):
-            version = line.split('=')[1].strip().strip('\'')
-            break
-if version is None:
-    raise RuntimeError('Could not determine version')
-
-
-DISTNAME = 'unbalanced-ot-functionals'
-DESCRIPTION = descr
-MAINTAINER = 'Thibault Séjourné'
-MAINTAINER_EMAIL = 'thibault.sejourne@ens.fr'
-LICENSE = 'MIT'
-DOWNLOAD_URL = 'https://github.com/thibsej/unbalanced-ot-functionals.git'
-VERSION = version
-URL = 'https://github.com/thibsej/unbalanced-ot-functionals'
-
-
-def package_tree(pkgroot):
-    """Get the submodule list."""
-    # Adapted from VisPy
-    path = os.path.dirname(__file__)
-    subdirs = [os.path.relpath(i[0], path).replace(os.path.sep, '.')
-               for i in os.walk(os.path.join(path, pkgroot))
-               if '__init__.py' in i[2]]
-    return sorted(subdirs)
-
-
-if __name__ == "__main__":
-    setup(name=DISTNAME,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          license=LICENSE,
-          version=VERSION,
-          url=URL,
-          download_url=DOWNLOAD_URL,
-          long_description=open('README.md').read(),
-          install_requires=[
+from setuptools import setup
+setup(
+    name="unbalancedot",
+    distname="",
+    version='0.0.1',
+    description="Functionals derived from the theory of entropically "
+                "regularized unbalanced optimal transport ",
+    author='Thibault Sejourne',
+    author_email='thibault.sejourne@ens.fr',
+    url='https://github.com/thibsej/unbalanced-ot-functionals',
+    packages=['unbalancedot', 'unbalancedot.tests'],
+    install_requires=[
+              'numpy',
+              'torch'
           ],
-          classifiers=[
-              'Intended Audience :: Science/Research',
-              'Intended Audience :: Developers',
-              'License :: OSI Approved',
-              'Programming Language :: Python',
-              'Topic :: Software Development',
-              'Topic :: Scientific/Engineering',
-              'Operating System :: Microsoft :: Windows',
-              'Operating System :: POSIX',
-              'Operating System :: Unix',
-              'Operating System :: MacOS',
-          ],
-          platforms='any',
-          packages=package_tree('unbalanced-ot-functionals'),
-          )
+    license="MIT",
+)
